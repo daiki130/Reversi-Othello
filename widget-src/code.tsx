42fb6e44e38aefea0ff1ebbf2d3bcd3fa496bada
ishida-daiki <ishida-daiki@dmm.com>
2024-09-18 18:46:49 +0900

    Initial commit

---
+++ widget-src/code.tsx
@@ -0,0 +1,47 @@
+const { widget } = figma;
+const {
+  // Components
+  AutoLayout,
+  Frame,
+  Text,
+  Input,
+  Rectangle,
+  Image,
+  SVG,
+  Ellipse,
+  Line,
+  Fragment,
+
+  // Hooks
+  useSyncedState,
+  useSyncedMap,
+  usePropertyMenu,
+  useEffect,
+  useStickable,
+  useStickableHost,
+  useWidgetId,
+
+  // Functions
+  register,
+  waitForTask,
+} = widget;
+
+function Widget() {
+  return (
+    <AutoLayout
+      direction="horizontal"
+      horizontalAlignItems="center"
+      verticalAlignItems="center"
+      height="hug-contents"
+      padding={8}
+      fill="#FFFFFF"
+      cornerRadius={8}
+      spacing={12}
+    >
+      <Text fontSize={32} horizontalAlignText="center">
+        hey
+      </Text>
+    </AutoLayout>
+  );
+}
+widget.register(Widget);
