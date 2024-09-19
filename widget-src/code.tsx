c6313aae791a4bc37dfe21e37a80b6f8e0dd753e
ishida-daiki <ishida-daiki@dmm.com>
2024-09-19 11:40:50 +0900

    環境を初期化

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,50 +1,22 @@
 const { widget } = figma;
-const {
-  // Components
-  AutoLayout,
-  Frame,
-  Text,
-  Input,
-  Rectangle,
-  Image,
-  SVG,
-  Ellipse,
-  Line,
-  Fragment,
-
-  // Hooks
-  useSyncedState,
-  useSyncedMap,
-  usePropertyMenu,
-  useEffect,
-  useStickable,
-  useStickableHost,
-  useWidgetId,
-
-  // Functions
-  register,
-  waitForTask,
-} = widget;
-
-import { OthelloGame } from "./components/othello-game";
+const { AutoLayout, Ellipse, Frame, Image, Rectangle, SVG, Text } = widget;
 
 function Widget() {
   return (
-    <OthelloGame />
-    // <AutoLayout
-    //   direction="horizontal"
-    //   horizontalAlignItems="center"
-    //   verticalAlignItems="center"
-    //   height="hug-contents"
-    //   padding={8}
-    //   fill="#FFFFFF"
-    //   cornerRadius={8}
-    //   spacing={12}
-    // >
-    //   <Text fontSize={32} horizontalAlignText="center">
-    //     hey
-    //   </Text>
-    // </AutoLayout>
+    <AutoLayout
+      direction="horizontal"
+      horizontalAlignItems="center"
+      verticalAlignItems="center"
+      height="hug-contents"
+      padding={8}
+      fill="#FFFFFF"
+      cornerRadius={8}
+      spacing={12}
+    >
+      <Text fontSize={32} horizontalAlignText="center">
+        test
+      </Text>
+    </AutoLayout>
   );
 }
 widget.register(Widget);
