dc402a74035b78ce86e1916168b04496a9b576f4
ishida-daiki <129714753+ishida-daiki@users.noreply.github.com>
2024-09-21 22:42:37 +0900

    feat: Refactor Widget component to improve code organization and readability

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,28 +1,15 @@
-const { widget, notify } = figma;
-const {
-  AutoLayout,
-  Text,
-  Frame,
-  Fragment,
-  Ellipse,
-  Line,
-  Rectangle,
-  Image,
-  Span,
-  Input,
-  SVG,
-  useSyncedState,
-  useSyncedMap,
-  useEffect,
-  usePropertyMenu,
-  useStickable,
-  useStickableHost,
-} = widget;
-import { Button } from "./components/Button";
-import { EllipseWithImage } from "./components/EllipseWithImage";
-import { Stone } from "./components/Stone";
-import { OthelloBoard } from "./components/board";
+const { widget, notify } = figma
+const { useSyncedState, AutoLayout, Text, usePropertyMenu, useSyncedMap} = widget
+
+import { Button } from "./components/Button"
+import { Stone } from "./components/Stone"
+import { EllipseWithImage } from "./components/EllipseWithImage"
+import { OthelloBoard } from "./components/board"
+
 function Widget() {
+  // const [players, setPlayers] = useSyncedState<Map<string, string>>("players", new Map())
+  const [gameStarted, setGameStarted] = useSyncedState("gameStarted", false)
+
   const [label, setLabel] = useSyncedState(
     "label",
     "👇 Pick a stone to start the game."
@@ -53,109 +40,68 @@ function Widget() {
     }
     if (players.size === 1) {
       setDescription("Waiting for 1 more player");
-      setButtonLabel("Join");
     }
     if (players.size === 2) {
       setDescription("Let's play!");
       setButtonLabel("Start Game");
     }
   };
-  usePropertyMenu(
-    [
-      {
-        itemType: "action",
-        tooltip: "Action",
-        propertyName: "action",
-      },
-      {
-        itemType: "separator",
-      },
-      {
-        itemType: "color-selector",
-        propertyName: "color-selector",
-        tooltip: "Color selector",
-        selectedOption: "#000",
-        options: [{ option: "#000", tooltip: "Black" }],
-      },
-      {
-        itemType: "dropdown",
-        propertyName: "dropdown",
-        tooltip: "Dropdown",
-        selectedOption: "option1",
-        options: [{ option: "option1", label: "Option 1" }],
-      },
-      {
-        itemType: "toggle",
-        tooltip: "Toggle",
-        propertyName: "toggle",
-        isToggled: true,
-      },
-      {
-        itemType: "link",
-        propertyName: "link",
-        tooltip: "link",
-        href: "www.google.com",
-      },
-      {
-        tooltip: "link with icon null",
-        propertyName: "Link",
-        itemType: "link",
-        href: "https://asana.com",
-        icon: null,
-      },
-    ],
-    ({ propertyName, propertyValue }) => {}
-  );
-  useStickable(async ({ newHostId, oldHostId }) => {
-    console.log(newHostId);
-    console.log(oldHostId);
-  });
+
+  function handleGameStart() {
+    if (players.size === 2) {
+      setGameStarted(true)
+    }
+  }
+
+  if (gameStarted) {
+    return <OthelloBoard />
+  }
 
   return (
-    <OthelloBoard />
-    // <AutoLayout
-    //   direction="vertical"
-    //   spacing={20}
-    //   padding={24}
-    //   minWidth={240}
-    //   verticalAlignItems="center"
-    //   horizontalAlignItems="center"
-    //   fill="#FFFFFF"
-    //   cornerRadius={10}
-    //   effect={{
-    //     type: "drop-shadow",
-    //     color: {
-    //       r: 0,
-    //       g: 0,
-    //       b: 0,
-    //       a: 0.3,
-    //     },
-    //     blur: 10,
-    //     offset: { x: 0, y: 0 },
-    //   }}
-    // >
-    //   {players.size < 2 && <Text>{label}</Text>}
-    //   <AutoLayout
-    //     direction="horizontal"
-    //     verticalAlignItems="center"
-    //     horizontalAlignItems="center"
-    //     spacing={32}
-    //     overflow="visible"
-    //   >
-    //     <Stone color="white" onClick={handleJoin} />
-    //     <Stone color="black" onClick={handleJoin} />
-    //   </AutoLayout>
-    //   {players.size === 2 ? (
-    //     <Button label={buttonLabel} onClick={() => console.log("Game Start")} />
-    //   ) : (
-    //     <Text>{description}</Text>
-    //   )}
-    //   <AutoLayout direction="horizontal" spacing={-4}>
-    //     {Array.from(players.values()).map((icon, index) => (
-    //       <EllipseWithImage key={index} src={icon as string} />
-    //     ))}
-    //   </AutoLayout>
-    // </AutoLayout>
-  );
+    <AutoLayout
+      direction="vertical"
+      spacing={20}
+      padding={24}
+      minWidth={240}
+      verticalAlignItems="center"
+      horizontalAlignItems="center"
+      fill="#FFFFFF"
+      cornerRadius={10}
+      effect={{
+        type: "drop-shadow",
+        color: {
+          r: 0,
+          g: 0,
+          b: 0,
+          a: 0.3,
+        },
+        blur: 10,
+        offset: { x: 0, y: 0 },
+      }}
+    >
+      {players.size < 2 && <Text>{label}</Text>}
+      <AutoLayout
+        direction="horizontal"
+        verticalAlignItems="center"
+        horizontalAlignItems="center"
+        spacing={32}
+        overflow="visible"
+      >
+        <Stone color="white" onClick={handleJoin} />
+        <Stone color="black" onClick={handleJoin} />
+      </AutoLayout>
+      {players.size === 2 ? (
+        <Button label={buttonLabel} onClick={handleGameStart} />
+      ) : (
+        <Text>{description}</Text>
+      )}
+      <AutoLayout direction="horizontal" spacing={-4}>
+        {Array.from(players.values()).map((icon, index) => (
+          <EllipseWithImage key={index} src={icon as string} />
+        ))}
+      </AutoLayout>
+    </AutoLayout>
+  )
 }
-widget.register(Widget);
+
+widget.register(Widget)
